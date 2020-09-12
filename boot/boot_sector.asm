86939791e78b5557a9d58caf9c368e82e73597e5
Elad Matia <elad.matia@gmail.com>
2020-09-12 17:18:21 +0300

    Added disk_load function to loaded sectors from disk

--- boot/boot_sector.asm
+++ boot/boot_sector.asm
@@ -8,9 +8,12 @@ jmp _text
 
 ; set data here
 data:
-    start_message: db 0x5b,"  OK  ",0x5d, " Booting in real mode...",0
+    start_message: db 0x5b,"  OK  ",0x5d, " Booting in real mode...", 0xD, 0xA, 0
+    BOOT_DRIVE: db 0
 
 _text:
+    mov [BOOT_DRIVE], dl ; save the boot drive
+
     ; setup stack:
     ; there is empty space from 0x00007e00 to 0x0007ffff (around 480kb). because the stack grows downwards,
     ; we will set the stack pointer to 0x80000, this way we will have our stack ready to go! see: https://wiki.osdev.org/Memory_Map_(x86)#Real_mode_address_space_.28.3C_1_MiB.29
@@ -19,11 +22,26 @@ _text:
 
     mov bx, start_message
     call print_realmode
-    mov dx, 0xCAFE
+    ; mov dx, 0xCAFE
+    ; call print_hex_realmode
+
+    ; temp:
+    mov bx, 0x9000 ; Load 5 sectors to 0x0000 (ES): 0x9000 (BX)
+    mov dh, 2
+    mov dl, [BOOT_DRIVE] ;from the boot disk.
+    call load_disk
+
+    mov dx, [0x9000] ; Print out the first loaded word
     call print_hex_realmode
+
+    mov dx, [0x9000+0x200] ; print the first word from the second sector (excluding the bootsector)
+    call print_hex_realmode 
 
     jmp $ ; jmp forever here
 
     times 510-($-$$) db 0 ;bootsectors are 512 bytes, last two bytes are magic numbers
 
     dw 0xaa55
+    ; The next sectors:
+    times 256 dw 0xCAFE 
+    times 256 dw 0xBABE
--- boot/realmode_lib.asm
+++ boot/realmode_lib.asm
@@ -30,7 +30,7 @@ print_hex_realmode:
         add ax, 0x7
         is_number:
             add ax, 0x30
-            mov byte [bx], al ; I sat for hours on a bug, we only want the right byte, not all of ax
+            mov byte [bx], al ; I sat for hours on a bug, we only want the lo byte, not all of ax
             dec bx
             cmp cx, 0
             jz print_hex
@@ -42,3 +42,29 @@ print_hex_realmode:
     ret
     str: db "0x0000",0
 
+; Load dh sectors to ES:BX from drive dl
+load_disk:
+    push dx
+
+    mov ah, 0x02 ; read sectors
+    mov al, dh
+    ; Start from CHS(0,0,2)
+    mov ch, 0x00; cylinder 0
+    mov dh, 0x00 ; head 0
+    mov cl, 0x02 ; read right after the boot sector, the first sector is 512 and is already loaded
+    int 0x13 ; Interrupt -> disk routines
+
+    jc disk_error ; if carry flag is on, something went wrong
+
+    pop dx ; restore dx, dh contains how many sectors we were asked to read
+    cmp dh, al ; al contain the sectors that were actually read
+    jne disk_error
+    ret
+
+    disk_error:
+        mov bx, DISK_ERROR_MESSAGE
+        call print_realmode
+        jmp $ ; hang here and do not proceed
+
+    ; variable
+    DISK_ERROR_MESSAGE: db "Error reading from disk!", 0xD, 0xA, 0
