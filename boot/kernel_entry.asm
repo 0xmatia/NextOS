6b172abb292d64db1cf28603b06bf60fde1e299d
Elad Matia <elad.matia@gmail.com>
2020-09-24 16:13:18 +0300

    Removed screen.h and screen.c from the drivers foders  and changed main to kmain

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -1,6 +1,6 @@
 [bits 32]
-[extern main] ; Define calling point. Must have same name as kernel.c 'main' function
+[extern kmain] ; Define calling point. Must have same name as kernel.c 'kmain' function
 global _start
 _start:
-    call main ; Calls the C function. The linker will know where it is placed in memory
+    call kmain ; Calls the C function. The linker will know where it is placed in memory
     jmp $
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,11 +1,17 @@
 #include "../cpu/isr.h"
-#include "../drivers/screen.h"
+#include "../drivers/screen/screen.h"
+#include "../libc/colors.h"
 
-void main()
+void kmain()
 {
     isr_install();
 
     clear_screen();
     kprint("Botzer Matia\n");
+    uint8_t green_on_black = generate_colorcode(GREEN, BLACK);
+    for (uint32_t i = 0; i<1000000; i++) {
+        kprint("Mivtzar is better than Apollo\n");
+        kprint_color("====================================\n", green_on_black);
+    }
     __asm__("int 13");
 }
